9d9fbad48fb363c62dd85955d09eae54581b0767
Bru Ronchi <brunom.ronchi@gmail.com>
2024-12-01 17:06:03 -0300

    Get sink name

--- scripts/code/vol.py
+++ scripts/code/vol.py
@@ -4,26 +4,26 @@ from sys import argv
 import re
 import subprocess
 
-def get_sink_data() -> tuple[int, bool, int]:
+def get_sink_data() -> tuple[int, bool, int, str]:
     p = subprocess.Popen(
         ['pulsemixer', '--list-sinks'],
         stdout=subprocess.PIPE,
         stderr=subprocess.STDOUT,
     )
 
-    id_, mute, vol = re.search(
-        r'ID: sink-(\d+).*?Mute: (\d).*?(\d+)%.*?Default',
+    id_, name, mute, vol = re.search(
+            r'ID: sink-(\d+), Name: (.*?), Mute: (\d).*?(\d+)%.*?Default',
         str(p.stdout.read(), encoding="utf8")
     ).groups()
 
-    return id_, mute == "1", int(vol)
+    return id_, mute == "1", int(vol), name
 
 if __name__ == "__main__":
     if len(argv) == 1:
-        _, mute, vol = get_sink_data()
-        print(f"{vol: 3} " + ("𝄽 " if mute else "𝅘𝅥𝅮 "))
+        _, mute, vol, name = get_sink_data()
+        print(f"{vol: 3} " + ("𝄽 " if mute else "𝅘𝅥𝅮 ") + f"{name} ")
     else:
-        id_, _, _ = get_sink_data()
+        id_, _, _, _ = get_sink_data()
         what_do = argv[1]
         if what_do in {"+", "-"}:
             key, value = "volume", f"{what_do}5%"
